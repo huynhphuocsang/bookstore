f630242b8033c72a217f0bc881fe32d1a61013fd
huynhphuocsang <huynhphuocsang23@gmail.com>
2021-08-25 17:38:42 +0700

    bookstore for final exam

---
+++ src/main/java/com/ptit/bookstore/BookstoreApplication.java
@@ -0,0 +1,15 @@
+package com.ptit.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.ptit")
+public class BookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+}
