1b6fa9e1055720849c47d1e8ed990e2197b80d38
huynhphuocsang <huynhphuocsang23@gmail.com>
2021-08-28 12:26:23 +0700

    update model

--- src/main/java/com/ptit/bookstore/BookstoreApplication.java
+++ src/main/java/com/ptit/bookstore/BookstoreApplication.java
@@ -2,10 +2,12 @@ package com.ptit.bookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.ptit")
+@EntityScan("com.ptit.model")
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
