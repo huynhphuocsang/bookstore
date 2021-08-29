f3bb9855b6109178b8e83992bcade5da8e8b6e88
huynhphuocsang <huynhphuocsang23@gmail.com>
2021-08-29 11:42:44 +0700

    update db

---
+++ src/main/java/com/ptit/model/Province.java
@@ -0,0 +1,51 @@
+package com.ptit.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import lombok.Data;
+
+@Entity
+@Table(name="province")
+@Data
+public class Province {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="province_id")
+	private long provinceId; 
+	
+	@Column(name="province_name")
+	@Size(max=45)
+	private String provinceName; 
+	
+	
+	@OneToMany(mappedBy = "province")
+	Set<District> setDistrict = new HashSet<District>(); 
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
