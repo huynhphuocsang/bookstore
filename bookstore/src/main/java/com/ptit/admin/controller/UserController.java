cfd5423a58a6f58985f77314f5bfb44c5bf9fecb
truong-lv <truonglv.2609@gmail.com>
2021-10-24 20:48:21 +0700

    update admin/customer page

--- bookstore/src/main/java/com/ptit/admin/controller/UserController.java
+++ bookstore/src/main/java/com/ptit/admin/controller/UserController.java
@@ -1,14 +1,51 @@
 package com.ptit.admin.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ptit.model.Book;
+import com.ptit.model.User;
+import com.ptit.service.UserService;
 
 @Controller
 @RequestMapping("/admin/customer")
 public class UserController {
+	@Autowired
+	UserService userService;
+	
 	@GetMapping()
-	public String getDashboard() {
+	public String getDashboard(Model model) {
+		model.addAttribute("user", new User());
+		return getUser(model, 1, "username", "asc");
+	}
+	
+	@GetMapping("/{pageNo}")
+	public String getUser(Model model, @PathVariable(value = "pageNo") int pageNo,
+			@RequestParam("sortField") String sortField, @RequestParam("sortDir") String sortDir) {
+		
+		int pageSize = 5;
+		int pageFirst = 1;
+		model.addAttribute("book", new Book());
+		Page<User> page = userService.findPaginated(pageNo, pageSize, sortField, sortDir);
+
+		List<User> listUser = page.getContent();
+		model.addAttribute("listUser", listUser);
+		model.addAttribute("sortField", sortField);
+		model.addAttribute("sortDir", sortDir);
+		model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
+
+		model.addAttribute("pageFirst", pageFirst);
+		model.addAttribute("currentPage", pageNo);
+		model.addAttribute("totalPage", page.getTotalPages());
+		model.addAttribute("totalItem", page.getTotalElements());
 		
 		return "admin/customers";
 	}
--- bookstore/src/main/java/com/ptit/service/UserService.java
+++ bookstore/src/main/java/com/ptit/service/UserService.java
@@ -1,7 +1,12 @@
 package com.ptit.service;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.ptit.model.Book;
 import com.ptit.model.User;
 
 public interface UserService {
@@ -17,4 +22,9 @@ public interface UserService {
 	public boolean checkExistPhoneInfo(String phone, String username); 
 	public boolean verifyOldPassword(String oldPassword, String username); 
 	public boolean updatePassword(String password, String username); 
+	
+	public Page<User> getAllUser(Pageable page); 
+	public List<User> findUser(String key); 
+	public Page<User> findPage(int pageNo, int pageSize);
+	public Page<User> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
 }
