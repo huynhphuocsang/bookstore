dc504fbe8da046e0c09d59038e3aaaf6f33caac3
truong-lv <truonglv.2609@gmail.com>
2021-11-09 22:43:59 +0700

    update Reviews manager pages

--- bookstore/src/main/java/com/ptit/admin/controller/ReviewController.java
+++ bookstore/src/main/java/com/ptit/admin/controller/ReviewController.java
@@ -1,5 +1,7 @@
 package com.ptit.admin.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,18 +10,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.ptit.exception.ResourceNotFoundException;
 import com.ptit.model.Book;
 import com.ptit.model.Category;
+import com.ptit.model.Review;
+import com.ptit.model.ReviewId;
+import com.ptit.repository.ReviewDao;
 import com.ptit.service.BookService;
+import com.ptit.service.ReviewService;
 
 @Controller
 @RequestMapping("/admin/review")
 public class ReviewController {
 	@Autowired
 	BookService bookService;
+	
+	@Autowired
+	ReviewService reviewService;
+	
+	@Autowired
+	ReviewDao reviewDao;
 	
 	@GetMapping()
 	public String getHomeReviews(Model model) {
@@ -30,21 +44,64 @@ public class ReviewController {
 	@GetMapping("/{pageNo}")
 	public String getReviews(Model model, @PathVariable(value = "pageNo") int pageNo,
 			@RequestParam("sortField") String sortField, @RequestParam("sortDir") String sortDir) {
-		int pageSize = 5;
+		int pageSize = 4;
 		int pageFirst = 1;
 		model.addAttribute("book", new Book());
 		Page<Book> page = bookService.findPaginated(pageNo, pageSize, sortField, sortDir);
 
 		List<Book> listBook = page.getContent();
+		List<List<int[]>> listBookStar = new ArrayList<List<int[]>>();
+		List<int[]> emptyStar=new ArrayList<int[]>();
+		emptyStar.add(new int[] {0,0});
+		for(Book ele: listBook) {
+			if(!reviewDao.getListStar(ele.getIdBook()).isEmpty()) {
+				listBookStar.add(reviewDao.getListStar(ele.getIdBook()));
+			}
+			else {
+				listBookStar.add(emptyStar);
+			}
+		}
+		listBookStar.forEach((ele)->{
+			System.out.println(ele.get(0)[0]+"--"+ele.get(0)[1]);
+		});
+		
+//		 listBookStar.add(reviewDao.getListStar(1));
+		
 		model.addAttribute("listBook", listBook);
+		model.addAttribute("listBookStar", listBookStar);
+		
 		model.addAttribute("sortField", sortField);
 		model.addAttribute("sortDir", sortDir);
 		model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
-
+		
 		model.addAttribute("pageFirst", pageFirst);
 		model.addAttribute("currentPage", pageNo);
 		model.addAttribute("totalPage", page.getTotalPages());
 		model.addAttribute("totalItem", page.getTotalElements());
 		return "/admin/review";
+	}
+	
+	
+	//=============BOOK REVIEW==================================
+	@GetMapping("/bookReviews/{idBook}")
+	public String getBookReview(Model model, @PathVariable("idBook") long idBook,
+								@RequestParam(value = "leng",defaultValue = "2") int leng) throws ResourceNotFoundException {
+		Book book=bookService.getBookById(idBook);
+		List<Review> listReview=reviewService.findListByLeng(idBook, leng);
+		int totalReview=reviewService.getAllReviewViaBook(book).size();
+		
+		model.addAttribute("leng", leng);
+		model.addAttribute("book", book);
+		model.addAttribute("listReview", listReview);
+		model.addAttribute("totalReview", totalReview);
+		return "admin/bookReviews";
+	}
+	
+	@PostMapping("/bookReviews/delete")
+	public String deleteReview(@RequestParam("idBook") long idBook,@RequestParam(value = "leng",defaultValue = "0") int leng,
+								@RequestParam("idUser") long idUser) throws ResourceNotFoundException {
+		ReviewId reviewId=new ReviewId(idUser,idBook);
+		reviewService.deleteById(reviewId);
+		return "redirect:/admin/review/bookReviews/"+idBook+"?leng="+ leng;
 	}
 }
--- bookstore/src/main/java/com/ptit/service/ReviewService.java
+++ bookstore/src/main/java/com/ptit/service/ReviewService.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 import com.ptit.model.Book;
 import com.ptit.model.Review;
+import com.ptit.model.ReviewId;
 import com.ptit.model.User;
 
 public interface ReviewService {
 	public List<Review> getAllReviewViaBook(Book book); 
 	public void addReview(User user, Book book, float star, String comments); 
 	public boolean checkReviewed(User user, Book book); 
+	
+	public List<Review> findListByLeng(long id, int leng);
+	public void deleteById(ReviewId id);
 }
